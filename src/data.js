5d5e6d8b99a4d8d7a3a5e2a8b078ee56d6b003dd
Thiago Mata <taiagomata@gmail.com>
2022-04-21 11:49:51 -0300

    Adicionando Produtos na viewport

--- src/data.js
+++ src/data.js
@@ -43,7 +43,7 @@ export const categories = [
 
 ]
 
-export const popuparProducts = [
+export const popularProducts = [
   {
     /* RASP */
     id:1,
@@ -52,37 +52,37 @@ export const popuparProducts = [
   {
     /* RASP 02 */
     id:2,
-    img: (require('./img/products/002.jpg'))
+    img: (require('./img/products/002.jpg')),
   },
   {
     /* DISPLAY */
     id: 3,
-    img : (require('./img/products/000.jpg'))
+    img : (require('./img/products/000.jpg')),
   },
   {
     /* ARDUINO 003 */
     id: 4,
-    img : (require('./img/products/003.jpg'))
+    img : (require('./img/products/003.jpg')),
   },
   {
     /* ARDUINO 004 */
     id: 5,
-    img : (require('./img/products/004.jpg'))
+    img : (require('./img/products/004.jpg')),
   },
   {
     /* NVIDEA */
     id: 6,
-    img : (require('./img/products/006.jpg'))
+    img : (require('./img/products/006.jpg')),
   },
   {
     /* ROBOT ARDUINO */
     id: 7,
-    img : (require('./img/products/005.jpg'))
+    img : (require('./img/products/005.jpg')),
   },
   {
     /* KIT ARDUINO */
     id: 8,
-    img : (require('./img/products/007.jpg'))
+    img : (require('./img/products/007.jpg')),
   },
 ]
 

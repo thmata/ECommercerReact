f65cc66c4f68621bc3221b0e2f29c451513d64e8
Thiago Mata <taiagomata@gmail.com>
2022-04-22 16:46:01 -0300

    Adicionado parte de NOTICIAS/EMAIL

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,7 +1,61 @@
+import { Send } from "@material-ui/icons";
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: #f5fafd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+const InputCotainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid;
+`;
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding: 20px;
+`;
+const Botao = styled.button`
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+`;
 
 const Newsletter = () => {
-  return <Container><Title></Title></Title></Container>;
+  return (
+    <Container>
+      <Title>Novidades</Title>
+      <Description>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </Description>
+      <InputCotainer>
+        <Input placeholder="Digite seu E-mail" />
+        <Botao>
+          <Send />
+        </Botao>
+      </InputCotainer>
+    </Container>
+  );
 };
 
 export default Newsletter;

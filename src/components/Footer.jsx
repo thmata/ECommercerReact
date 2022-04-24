ca59c2989d943055c6dcfe3d0fd2690a7b503c84
Thiago Mata <taiagomata@gmail.com>
2022-04-24 00:36:56 -0300

    Avançando no footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,107 @@
+import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p``;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>asdas</Logo>
+        <Desc>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Arduino</ListItem>
+          <ListItem>Raspberry</ListItem>
+          <ListItem>NVidia</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Terms</ListItem>
+          <ListItem>Order Tracking</ListItem>
+        </List>
+      </Center>
+
+      <Right></Right>
+    </Container>
+  );
+};
+
+export default Footer;
